a309d9fec1c78535dfde3b43105cbf62b8f5905b
Jayavel Rajaa VR <jayavelrajaavr@gmail.com>
2025-12-13 14:18:17 +0530

    Add files via upload

---
+++ operators_basic/q1.java
@@ -0,0 +1,9 @@
+package operators_basic;
+//sum
+public class q1 {
+public static void main(String[] args){
+    int a=10;
+    int b=20;
+    System.out.println(a+b);    
+}
+}
---
+++ operators_basic/q5.java
@@ -0,0 +1,9 @@
+package operators_basic;
+//remainder-modulo division
+public class q5 {
+public static void main(String[] args){
+    int a=10;
+    int b=4;
+    System.out.println(a%b);    
+}
+}
